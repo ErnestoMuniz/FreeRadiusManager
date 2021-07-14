53cdffa2f7fe23f3a4c5bfc17d687a313c463d34
Ernesto Muniz <ernestotrapia@gmail.com>
2021-07-14 23:15:13 +0000

    Bugfix charts

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -41,42 +41,6 @@
             'wrapper'    => ['class' => 'col-md-6']
           ]
         ]);
-
-    // SERVER STATUS
-    $apache = ucfirst(explode(" ", exec('service apache2 status | grep Active'))[6]);
-    if ($apache == "Active") {
-      $ap_class = 'card text-white bg-success mb-2 text-center';
-      $apache = "$apache <i class='las la-check'></i>";
-    } else {
-      $ap_class = 'card text-white bg-danger mb-2 text-center';
-      $apache = "$apache <i class='las la-exclamation'></i>";
-    }
-
-    $hdd = str_replace("%", "", exec("df | awk '/ \/$/{print $5}'"));
-    if ($hdd <= 60) {
-      $hdd_class = 'card text-white bg-success mb-2 text-center';
-      $hdd = "$hdd% <i class='las la-check'></i>";
-    } else {
-      $hdd_class = 'card text-white bg-danger mb-2 text-center';
-      $hdd = "$hdd% <i class='las la-exclamation'></i>";
-    }
-
-    Widget::add()->to('before_content')
-        ->type('div')
-        ->class('row col-md-12 ml-1')
-        ->content([
-          [
-            'type'          => 'progress',
-            'class'         => $ap_class,
-            'value'         => $apache,
-            'description'   => 'Apache Status.',
-          ],[
-            'type'          => 'progress',
-            'class'         => $hdd_class,
-            'value'         => $hdd,
-            'description'   => 'Disk Utilization.',
-          ]
-        ]);
 @endphp
 
 @section('content')
